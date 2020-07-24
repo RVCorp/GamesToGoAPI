30750c5f113c465f5a70da1cd05e6be6b937f97c
Diego <driveror31@gmail.com>
2020-07-23 22:18:38 -0500

    GameImage Migration

---
+++ GamesToGoAPI/Migrations/20200724031546_GameImage.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GamesToGoAPI.Migrations
+{
+    public partial class GameImage : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Image",
+                table: "Game",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Image",
+                table: "Game");
+        }
+    }
+}
